e179b9b60f449a6a2c90f6139e4f9e46bd250df5
Raul Espinoza M <raul.espmol@gmail.com>
2026-07-23 18:31:31 -0400

    Update estados.services.ts
    Ordenar estados

--- src/catalogos/estados/estados.services.ts
+++ src/catalogos/estados/estados.services.ts
@@ -4,7 +4,8 @@ import type { Estado } from "./estado.type";
 export const obtenerEstados = async (): Promise<Estado[]> => {
   const { data, error } = await supabase
     .from("estados")
-    .select("id, key, label");
+    .select("id, key, label")
+    .order("orden", {ascending: true})
 
   if (error) throw error;
 
